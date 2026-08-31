acf9f93ab499dd2d2dbc99d95e4c51b109c3ce9c
Ƴunior Ƥortal <35150859+portyu9@users.noreply.github.com>
2026-08-31 13:05:36 -0400

    test: emit scoped Jest coverage evidence

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,13 @@ module.exports = {
   restoreMocks: true,
   testMatch: ['<rootDir>/tests/**/*.test.js'],
   testPathIgnorePatterns: ['<rootDir>/tests/e2e/'],
+  collectCoverageFrom: [
+    '<rootDir>/config/**/*.js',
+    '<rootDir>/mock/**/*.js',
+    '<rootDir>/src/**/*.js',
+    '!<rootDir>/src/pages/**',
+    '!<rootDir>/src/testing/**',
+  ],
+  coverageDirectory: '<rootDir>/reports/jest-coverage',
+  coverageReporters: ['text-summary', 'json-summary', 'cobertura'],
 };
